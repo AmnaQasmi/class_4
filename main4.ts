f760d263b736b66e86663fe97c5bfde56e81c539
AmnaQasmi <drakshanaltaf@gmail.com>
2023-12-06 21:54:52 +0500

    Initial commit

---
+++ main4.ts
@@ -0,0 +1,135 @@
+// let firstName = "Fatima";
+// let lastName = "Qasmi";
+// let firstName = "Urma";
+// let lastName = "Siddiqui";
+
+
+
+//               // True                  // False 
+// if (firstName === "Fatima" || lastName === "Qasmi" ) {
+//     console.log("You're not allowed to use my things")
+// }
+// else {
+//     console.log("You're allowed")
+// }
+
+// create function or call function.
+// this is how you can call the function but first you should have to make it then you can easily ues it by calling it as you can see it.
+// function greet() {
+//     console.log("Hello Hamzah")
+// }
+
+// function sum() {
+//     console.log(2 + 2)
+// }
+// greet();
+// sum();
+
+// this is how you can greet && to your  favrouit name or any other name ,person by just calling the function and  by writing the name. 
+//  function greet(name: string) {
+//         console.log("Hello", name);
+//     }
+// greet("Hamzah");
+// greet("Ali");
+// greet("Cristiano Ronaldo CR7");
+
+// this is how you add || subtract || multiply || divide the numbers.
+// function sum(num1:number, num2:number){
+//     console.log(num1 + num2)
+// }
+// sum(5 , 10);
+
+// function sum(num1: number, num2: number) {
+//     console.log(num1 + num2);
+//     return num1 + num2;
+
+// }
+//  the ans is in addition form 15.
+// let ans = sum(5, 10); // 15
+
+// this is how you can  multiply the numbers .
+//  function multiply(num1: number) {
+//         console.log(num1 * 2)
+//     }
+//             //15
+//     multiply(3);
+
+//Now we are talking about Arrays ; Arrays are basically this  [ ] .
+// let fruit1 = "Apple";
+// let fruit2 = "Orange";
+// let fruit3 = "Banana";
+// let fruit4 = "Pineapple";
+// console.log([fruit1,fruit2,fruit3,fruit4]);
+
+// let subjects:string[] = [ "Math", "Science", "English", "History"];
+// console.log(subjects);
+
+// let ages:number[] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// console.log (ages);
+
+// let a = [true, false, true];
+// console.log(a);
+
+
+//these were count from 0 and are called index number :
+//               0         1         2          3  // Index number
+// let fruits = ["Apple", "Orange", "Banana", "Pineapple"];
+// this is how you can call || address any thing by using the index number by consoling and by putting the index number in an array :
+// console.log(fruits[1]);
+// console.log(fruits[2], fruits[3] );
+// let fruits = ["Apple", "Orange", "Banana", 4];
+// console.log(fruits[2], fruits[3] );
+
+// pop can pop || eliminate the last element from your array; as you can see if you run the program on cmd.
+// let menu = ["Kofta", "Kheer", "Biryani", "Gajar ka Halwa"];
+
+// let myFirstPopped = menu.pop();
+
+// console.log(myFirstPopped);
+
+// the element pop || eleminate from the menu we don't like so this is how you can pust the new element in at the end of the array by using[. pop ] ;
+// let pushedReturn = menu.push("Chicken Tikka", "Chicken Karahi");
+
+// console.log(pushedReturn); it's index number will be print 
+
+// menu.pop();
+// console.log(menu);
+
+// this is how you can add a new length || new element at the starting to your array by using [. unshift ] ; 
+// let menu = ["Kofta", "Kheer", "Biryani", "Gajar ka Halwa"];//.1
+
+// let len  = menu.unshift("Chicken Tikka");//.2
+// menu.unshift();//.3
+// console.log(menu);//.4
+
+// this is how you can remove || shift the first element by using [. shift];
+// let menu = ["Kofta", "Kheer", "Biryani", "Green Chicken Pulao", "Gajar ka Halwa"];
+// menu.shift();
+// console.log(menu);
+
+// this is how you can cut the peice as per your choice through [. slice ] function from your array ;
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(1, 4);
+
+// console.log(citrus);
+
+//this is how you can inbsert the new element when evey you want as per your choice through [. splice ] function from your array ;
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let a = fruits.splice(0, 4, "Lemon", "Kiwi");
+
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
